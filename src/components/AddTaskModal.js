d513e319036ea3711bfb6bc2b6b7ec1e88f347f0
rituneosoft <ritu.dobhal@neosoftmail.com>
2025-06-23 21:23:45 +0530

    input message change again

--- src/components/AddTaskModal.js
+++ src/components/AddTaskModal.js
@@ -41,7 +41,7 @@ const AddTaskModal = ({ selectedTask, showModal, handleClose }) => {
     setTask((prevTask) => ({ ...prevTask, [name]: value }));
   }, []);
 
-  // Validate form input
+  // use Validate form input
   const validateForm = useCallback(() => {
     let newErrors = {};
 
